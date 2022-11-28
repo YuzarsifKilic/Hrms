8f25ffde02507ba6170543a071b5fe23c11ec370
Yusuf Kilic <yuzarsifkilic@gmail.com>
2022-11-28 20:38:08 +0300

    update the employer manager

--- src/main/java/com/example/hrmsproject/business/abstracts/EmployerService.java
+++ src/main/java/com/example/hrmsproject/business/abstracts/EmployerService.java
@@ -1,12 +1,11 @@
 package com.example.hrmsproject.business.abstracts;
 
+import com.example.hrmsproject.core.Result;
 import com.example.hrmsproject.entities.concretes.Employer;
 
 import java.util.List;
 
 public interface EmployerService {
     List<Employer> getEmployerList();
-    boolean saveEmployer(Employer employer);
-
-    String getEmployerMessage(Employer employer);
+    Result saveEmployer(Employer employer);
 }
--- src/main/java/com/example/hrmsproject/business/concretes/EmployerManager.java
+++ src/main/java/com/example/hrmsproject/business/concretes/EmployerManager.java
@@ -3,6 +3,9 @@ package com.example.hrmsproject.business.concretes;
 import com.example.hrmsproject.business.abstracts.ConfirmsEmployerService;
 import com.example.hrmsproject.business.abstracts.EmployerService;
 import com.example.hrmsproject.business.abstracts.EmployerVerificationCodeService;
+import com.example.hrmsproject.core.ErrorResult;
+import com.example.hrmsproject.core.Result;
+import com.example.hrmsproject.core.SuccessResult;
 import com.example.hrmsproject.dataAccess.abstracts.EmployerDao;
 import com.example.hrmsproject.entities.concretes.Employer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,43 +43,36 @@ public class EmployerManager implements EmployerService {
     }
 
     @Override
-    public boolean saveEmployer(Employer employer) {
-       return websiteAndEmailCompatibility(employer).equals(Verification.Success);
+    public Result saveEmployer(Employer employer) {
+        if (websiteAndEmailCompatibility(employer).isSuccess()) {
+            this.employerDao.save(employer);
+        }
+       return websiteAndEmailCompatibility(employer);
     }
 
-    @Override
-    public String getEmployerMessage(Employer employer) {
-        return websiteAndEmailCompatibility(employer);
-    }
-
-    private String checkTheVerifications(Employer employer) {
-        return websiteAndEmailCompatibility(employer);
-    }
-
-    private String websiteAndEmailCompatibility(Employer employer) {
+    private Result websiteAndEmailCompatibility(Employer employer) {
         String websiteDomain[] = employer.getWebSite().split("\\.");
         String email[] = employer.getEmail().split("@");
         String companyName[] = email[1].split("\\.");
 
-        if (websiteDomain[1].equals(companyName[0])) {
-
-            return checkTheVerificationCode(employer).equals(returnVerification(Verification.Success)) ? returnVerification(Verification.Success) : checkTheVerificationCode(employer);
+        if (!websiteDomain[1].equals(companyName[0])) {
+            return new ErrorResult(returnVerification(Verification.WebsiteAndEmailCompatibility));
         }
-        return returnVerification(Verification.WebsiteAndEmailCompatibility);
+        return checkTheVerificationCode(employer);
     }
 
-    private String checkTheVerificationCode(Employer employer) {
+    private Result checkTheVerificationCode(Employer employer) {
         if(!this.employerVerificationCodeService.getVerificationCode(employer.getId()).isVerified()) {
-            return returnVerification(Verification.VerificationCode);
+            return new ErrorResult(returnVerification(Verification.VerificationCode));
         }
-        return checkTheConfirm(employer).equals(returnVerification(Verification.Success)) ? returnVerification(Verification.Success) : checkTheConfirm(employer);
+        return checkTheConfirm(employer);
     }
 
-    private String checkTheConfirm(Employer employer) {
+    private Result checkTheConfirm(Employer employer) {
         if (!this.confirmsEmployerService.getConfirmsForEmployer(employer.getId()).isConfirmed()) {
-            return returnVerification(Verification.Confirm);
+            return new ErrorResult(returnVerification(Verification.Confirm));
         }
-        return returnVerification(Verification.Success);
+        return new SuccessResult(returnVerification(Verification.Success));
     }
 
     private enum Verification {
